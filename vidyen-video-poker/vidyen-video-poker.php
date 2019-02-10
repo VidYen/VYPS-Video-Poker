4405b6b7195e083d2c36514480807ff2e1d5c45c
Felty <40067341+VidYen@users.noreply.github.com>
2019-02-09 22:04:47 -0500

    $wpdb update

--- vidyen-video-poker/vidyen-video-poker.php
+++ vidyen-video-poker/vidyen-video-poker.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://vidyen.com
  * Description: A video poker game for the VidYen Point System.
  * Author: VidYen, LLC
- * Version: 0.0.23
+ * Version: 0.0.26
  * Author URI: http://VidYen.com
  * Text Domain: vidyen-video-poker
  * License: GPLv2
@@ -48,9 +48,9 @@ function videyen_video_poker_sql_install() {
 		//NOTE: I have the mind to make mediumint to int, but I wonder if you get 8 million log transactios that you should consider another solution than VYPS.
 
 		//videyen_video_poker table creation
-    $table_name_points = $wpdb->prefix . 'videyen_video_poker';
+    $table_name_poker = $wpdb->prefix . 'videyen_video_poker';
 
-    $sql = "CREATE TABLE {$table_name_points} (
+    $sql = "CREATE TABLE {$table_name_poker} (
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
 		point_id mediumint(9) NOT NULL,
 		maximum_bet mediumint(9) NOT NULL,
@@ -60,4 +60,106 @@ function videyen_video_poker_sql_install() {
     require_once (ABSPATH . 'wp-admin/includes/upgrade.php'); //I never did investigate why the original outsource dev used this.
 
     dbDelta($sql);
+
+		//Default data
+		$data_insert = [
+				'point_id' => 1,
+				'maximum_bet' => 1000,
+		];
+
+		$wpdb->insert($table_name_poker, $data_insert);
+}
+
+//adding menues
+add_action('admin_menu', 'vidyen_video_poker_menu');
+
+function vidyen_video_poker_menu()
+{
+    $parent_page_title = "VidYen Video Poker";
+    $parent_menu_title = 'VY Video Poker';
+    $capability = 'manage_options';
+    $parent_menu_slug = 'vidyen_video_poker';
+    $parent_function = 'vidyen_video_poker_menu_page';
+    add_menu_page($parent_page_title, $parent_menu_title, $capability, $parent_menu_slug, $parent_function);
+}
+
+//The actual menu
+function vidyen_video_poker_menu_page()
+{
+	global $wpdb;
+
+	if (isset($_POST['point_id']))
+	{
+		//ID Text value
+		$point_id = intval($_POST['point_id']); //Even though I am in the believe if an admin sql injects himself, we got bigger issues, but this has been sanitized.
+
+		//The icon. I'm suprised this works so well
+		$max_bet =intval($_POST['max_bet']);
+
+    $table_name_poker = $wpdb->prefix . 'videyen_video_poker';
+
+	    $data = [
+	        'point_id' => $point_id,
+	        'maximum_bet' => $max_bet,
+	    ];
+
+			$wpdb->update($table_name_poker, $data, ['id' => 1]);
+	    //$data_id = $wpdb->update($table_name_poker , $data);
+
+	    //I forget thow this works
+	    $message = "Added successfully.";
+	}
+
+	//the $wpdb stuff to find what the current name and icons are
+	$table_name_poker = $wpdb->prefix . 'videyen_video_poker';
+
+	$first_row = 1; //Note sure why I'm setting this.
+
+	//Point_id pull
+	$point_id_query = "SELECT point_id FROM ". $table_name_poker . " WHERE id= %d"; //I'm not sure if this is resource optimal but it works. -Felty
+	$point_id_query_prepared = $wpdb->prepare( $point_id_query, $first_row );
+	$point_id = $wpdb->get_var( $point_id_query_prepared );
+
+	//Point_id pull
+	$max_bet_query = "SELECT maximum_bet FROM ". $table_name_poker . " WHERE id= %d"; //I'm not sure if this is resource optimal but it works. -Felty
+	$max_bet_query_prepared = $wpdb->prepare( $max_bet_query, $first_row );
+	$max_bet = $wpdb->get_var( $max_bet_query_prepared );
+
+
+	//Just setting to 1 if nothing else I suppose, but should always be something
+	if ($point_id == '')
+	{
+		$point_id = 1;
+	}
+
+	//Just setting to 1 if nothing else I suppose, but should always be something
+	if ($max_bet == '')
+	{
+		$max_bet = 1000;
+	}
+
+	//It's possible we don't use the VYPS logo since no points.
+  $vyps_logo_url = plugins_url( 'includes/images/logo.png', __FILE__ );
+	//Logo from base. If a plugin is installed not on the menu they can't see it not showing.
+	echo '<br><br><img src="' . $vyps_logo_url . '" > ';
+
+	//Static text for the base plugin
+	echo
+	'<h1>VidYen Video Poker Sub-Plugin</h1>
+	<p>The Video poker!</p>
+	<table>
+		<form method="post">
+			<tr>
+				<th>Point ID</th>
+				<th>Max Bet</th>
+				<th>Submit</th>
+			</tr>
+			<tr>
+				<td><input type="number" name="point_id" type="number" id="point_id" min="1" step="1" value="' . $point_id .  '" required="true"></td>
+				<td><input type="number" name="max_bet" type="number" id="max_bet" min="1" max="1000000" step="1" value="' . $max_bet . '" required="true"></td>
+				<td><input type="submit" value="Submit"></td>
+			</tr>
+		</form>
+	</table>
+	';
 }
